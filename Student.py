f93c5c522034203b1568c9764541f1261922cb75
Steve Yeung <z5421892@ad.unsw.edu.au>
2024-08-04 20:07:04 +1000

    created student class

---
+++ Student.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, name, year, period, data):
+        self.name = name
+        self.year = year
+        self.period = period
+        self.data = data
+
+    def add_grades(self):
+        # todo:
+        pass
+
+    def generate_pdf(self):
+        # todo:
+        pass
