4e23f67220dbcfd70d1cbaeb77e65e94c5827040
Steve Yeung <z5421892@ad.unsw.edu.au>
2024-08-30 19:01:49 +1000

    created method to create single report

--- src/main.py
+++ src/main.py
@@ -15,7 +15,31 @@ def generate_year_reports(year: Year):
 
 
 def main():
-    csv_file = 'src/csv_store/24t3y10.csv'
+    y10_csv_file = 'src/csv_store/24t3y10.csv'
+    y9_csv_file = 'src/csv_store/24t3y9.csv'
+    y8_csv_file = 'src/csv_store/24t3y8.csv'
+    y7_csv_file = 'src/csv_store/24t3y7.csv'
+
+    response = input("Do you want a single student report? y/n: ")
+    if response == 'y':
+        stu_name = input("Enter the student's name: ")
+        stu_year = input("Enter the student's year: 7/8/9/10 ")
+        if stu_year == '7':
+            csv_file = y7_csv_file
+        elif stu_year == '8':
+            csv_file = y8_csv_file
+        elif stu_year == '9':
+            csv_file = y9_csv_file
+        elif stu_year == '10':
+            csv_file = y10_csv_file
+
+        year = csv_to_object(csv_file)
+        for class_ in year.get_classes():
+            for s in class_.get_students():
+                if s.get_name() == stu_name:
+                    generate_pdf(year.get_year(), class_.get_tutor(), f"Y{year.get_year()} {class_.get_level()}",
+                                 class_.get_topics(), s, year.get_period(), s.get_days_away())
+                    return
 
     # Read a CSV file and generate a new Year object
     year = csv_to_object(csv_file)
